0c81b1bdc2e2957e8eace03bc059d55323d76fae
bgpat <bgpat23@gmail.com>
2016-11-25 13:25:27 +0900

    Split

---
+++ request.go
@@ -0,0 +1,37 @@
+package twitter
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func Decode(resp *http.Response, data interface{}) error {
+	if err := NewErrors(resp); err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(data)
+}
+
+func (c *Client) ParseURL(path string, params url.Values) string {
+	for k, v := range params {
+		if len(k) <= 0 || len(v) <= 0 || k[0] != ':' {
+			continue
+		}
+		path = strings.Replace(path, k, strings.Join(v, ","), -1)
+		delete(params, k)
+	}
+	return fmt.Sprintf(c.URLFormat, path)
+}
+
+func (c *Client) GET(path string, params url.Values, data interface{}) error {
+	uri := c.ParseURL(path, params)
+	resp, err := c.OAuthClient.Get(nil, c.AccessToken, uri, params)
+	if err != nil {
+		return err
+	}
+	return Decode(resp, data)
+}
--- twitter.go
+++ twitter.go
@@ -1,22 +1,16 @@
 package twitter
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
-	"net/url"
-	"strings"
 
 	"github.com/garyburd/go-oauth/oauth"
 )
 
 type Client struct {
-	OAuthClient   oauth.Client
-	ConsumerToken *oauth.Credentials
-	AccessToken   *oauth.Credentials
-	RequestToken  *oauth.Credentials
-	URLFormat     string
+	OAuthClient  oauth.Client
+	AccessToken  *oauth.Credentials
+	RequestToken *oauth.Credentials
+	URLFormat    string
 }
 
 func NewClient(consumer, token *oauth.Credentials) *Client {
@@ -27,18 +21,9 @@ func NewClient(consumer, token *oauth.Credentials) *Client {
 			TokenRequestURI:               "https://api.twitter.com/oauth/access_token",
 			Credentials:                   *consumer,
 		},
-		ConsumerToken: consumer,
-		AccessToken:   token,
-		URLFormat:     "https://api.twitter.com/1.1/%s.json",
+		AccessToken: token,
+		URLFormat:   "https://api.twitter.com/1.1/%s.json",
 	}
-}
-
-func Decode(resp *http.Response, data interface{}) error {
-	if err := NewErrors(resp); err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return json.NewDecoder(resp.Body).Decode(data)
 }
 
 func (c *Client) RequestTokenURL(callback string) (string, error) {
@@ -59,24 +44,4 @@ func (c *Client) GetAccessToken(verifier string) error {
 	c.AccessToken = token
 	log.Printf("%#v\n", resp)
 	return nil
-}
-
-func (c *Client) ParseURL(path string, params url.Values) string {
-	for k, v := range params {
-		if len(k) <= 0 || len(v) <= 0 || k[0] != ':' {
-			continue
-		}
-		path = strings.Replace(path, k, strings.Join(v, ","), -1)
-		delete(params, k)
-	}
-	return fmt.Sprintf(c.URLFormat, path)
-}
-
-func (c *Client) GET(path string, params url.Values, data interface{}) error {
-	uri := c.ParseURL(path, params)
-	resp, err := c.OAuthClient.Get(nil, c.AccessToken, uri, params)
-	if err != nil {
-		return err
-	}
-	return Decode(resp, data)
 }
