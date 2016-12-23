6b766bfb7813b1029a7932323db4c9335b06e920
bgpat <bgpat23@gmail.com>
2016-12-24 08:05:19 +0900

    Add streaming request

---
+++ stream.go
@@ -0,0 +1,64 @@
+package twtr
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+)
+
+type Stream struct {
+	*http.Response
+	Data chan string
+}
+
+func (c *Client) NewStream(method, urlStr string, params Values) (*Stream, error) {
+	var resp *http.Response
+	var err error
+	if method == "GET" {
+		resp, err = c.getRequest(urlStr, params)
+	} else if method == "POST" {
+		resp, err = c.postRequest(urlStr, params)
+	} else {
+		return nil, errors.New(method + " is not implemented method")
+	}
+	if err != nil {
+		return nil, err
+	}
+	ch := make(chan string)
+	stream := &Stream{
+		Response: resp,
+		Data:     ch,
+	}
+	go stream.worker()
+	return stream, nil
+}
+
+func (s *Stream) Close() {
+	s.Response.Body.Close()
+	ch := make(chan struct{}, 0)
+	s.Response.Request.Cancel = ch
+	close(ch)
+	s.Data = nil
+}
+
+func (s *Stream) Decode(data interface{}) error {
+	for {
+		if s.Data == nil {
+			return errors.New("stream is not initialized")
+		}
+		str := <-s.Data
+		if str == "" {
+			continue
+		}
+		return json.NewDecoder(strings.NewReader(str)).Decode(data)
+	}
+}
+
+func (s *Stream) worker() {
+	scanner := bufio.NewScanner(s.Response.Body)
+	for scanner.Scan() {
+		s.Data <- scanner.Text()
+	}
+}
