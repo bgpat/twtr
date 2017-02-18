f402a49c2864e04be7dffd6bb2cef2aea9e06607
bgpat <bgpat23@gmail.com>
2017-02-18 22:58:49 +0900

    Implement response type

---
+++ response.go
@@ -0,0 +1,69 @@
+package twtr
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+)
+
+type Response struct {
+	*http.Response
+	*Limit
+}
+
+type Responser interface {
+	ParseResponse(*http.Response) error
+}
+
+func (r *Response) ParseResponse(resp *http.Response) error {
+	r.Response = resp
+	return r.parseLimit()
+}
+
+func (r *Response) parseLimit() error {
+	l := Limit{}
+
+	limit, err := r.getHeader("X-Rate-Limit-Limit")
+	if err != nil {
+		return err
+	}
+	l.Limit, err = strconv.Atoi(limit)
+	if err != nil {
+		return err
+	}
+
+	remaining, err := r.getHeader("X-Rate-Limit-Remaining")
+	if err != nil {
+		return err
+	}
+	l.Remaining, err = strconv.Atoi(remaining)
+	if err != nil {
+		return err
+	}
+
+	reset, err := r.getHeader("X-Rate-Limit-Reset")
+	if err != nil {
+		return err
+	}
+	err = l.Reset.ParseUnixTime(reset)
+	if err != nil {
+		return err
+	}
+
+	r.Limit = &l
+	return nil
+}
+
+func (r *Response) getHeader(name string) (string, error) {
+	if r.Response.Header == nil {
+		return "", errors.New("header is nil")
+	}
+	v, ok := r.Response.Header[name]
+	if !ok {
+		return "", errors.New(name + " header is undefined")
+	}
+	if len(v) < 1 {
+		return "", errors.New(name + "header is empty")
+	}
+	return v[0], nil
+}
