7864473f99f97967c8728a1dcba64cd470d69e26
bgpat <bgpat23@gmail.com>
2017-02-19 10:51:20 +0900

    Add comment for godoc

--- friends.go
+++ friends.go
@@ -1,6 +1,8 @@
 package twtr
 
-func (c *Client) GetFriendsIDs(params *Params) (*UserIDs, *Response, error) {
+// GetFriendIDs returns a cursored collection of user IDs for every user the specified user is following (otherwise known as their “friends”).
+// https://dev.twitter.com/rest/reference/get/friends/ids
+func (c *Client) GetFriendIDs(params *Params) (*UserIDs, *Response, error) {
 	ids := new(UserIDs)
 	resp, err := c.GET("friends/ids", params, &ids)
 	return ids, resp, err
