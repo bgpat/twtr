43578ae971b339e43865dd00767ae1e8352f148d
bgpat <bgpat23@gmail.com>
2017-02-19 10:53:20 +0900

    Implement friends/list

--- friends.go
+++ friends.go
@@ -4,6 +4,14 @@ package twtr
 // https://dev.twitter.com/rest/reference/get/friends/ids
 func (c *Client) GetFriendIDs(params *Params) (*UserIDs, *Response, error) {
 	ids := new(UserIDs)
-	resp, err := c.GET("friends/ids", params, &ids)
+	resp, err := c.GET("friends/ids", params, ids)
 	return ids, resp, err
+}
+
+//GetFriendList returns a cursored collection of user objects for every user the specified user is following (otherwise known as their “friends”).
+// https://dev.twitter.com/rest/reference/get/friends/list
+func (c *Client) GetFriendList(params *Params) (*UserList, *Response, error) {
+	list := new(UserList)
+	resp, err := c.GET("friends/list", params, list)
+	return list, resp, err
 }
